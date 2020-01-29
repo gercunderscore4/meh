5bc456642e28fbd65d9a90f42d4efec332bb47b6
Geoffrey Card <gercunderscore4@gmail.com>
2020-01-28 22:34:44 -0500

    added some basic linux for later

--- meh_presets.bat
+++ meh_presets.bat
@@ -1,4 +1,6 @@
-REM Sample script for easily dragging and dropping folders for viewing.
+REM #!/bin/bash
+
+REM Example script for easily dragging and dropping folders for viewing.
 REM Auto-play with a 15s delay
 REM Zoom image to fill screen without distortion
 REM Recusively look through folders
@@ -8,3 +10,6 @@ REM At 0 x 0 in top left corner
 
 REM Windows version of the command
 start python "%~dp0\meh.py" %1 -d 15 -z -r -R -g "960x1080+0+0"
+
+REM # Linux version of the command (untested)
+REM python meh.py" "$1" -d 15 -z -r -R -g "910x930+0+0" &
