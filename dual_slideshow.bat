3f34f5cccdc82bd0926d44589801c72c916fdbd5
Geoffrey Card <gercunderscore4@gmail.com>
2020-01-29 20:47:22 -0500

    added a second script for dual slideshows

---
+++ dual_slideshow.bat
@@ -0,0 +1,2 @@
+START PowerShell -WindowStyle Hidden -Command py '%~dp0meh.py' -a -d 13 -r -R -z -g 960x1080+0+0   '%1'
+START PowerShell -WindowStyle Hidden -Command py '%~dp0meh.py' -a -d 17 -r -R -z -g 960x1080+960+0 '%1'
